1b75c88f1d6cf6bf278b1db99060ea3b1ab246e8
CemilOzer <cemilozer73@gmail.com>
2023-10-23 00:48:18 +0300

    init

---
+++ src/test/java/runners/ParalelRun2.java
@@ -0,0 +1,26 @@
+package runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {"html:target/cucumber-reports2.html",
+                "json:target/json-reports/cucumber2.json",
+                "junit:target/xml-report/cucumber2.xml"},
+        features = "src/test/resources/features",
+        glue = "stepdefinitions",
+        tags = "@smoke",
+        dryRun = false
+)
+public class ParalelRun2 {
+
+    /*plugin = {"html:target/cucumber-reports2.html",======>cucumber-reports2.html rapor ismini istedigimiz basligi koyabiliriz.
+                "json:target/json-reports/cucumber2.json",
+                "junit:target/xml-report/cucumber2.xml"},
+        features = "src/test/resources/features",
+        glue = "stepdefinitions",
+        tags = "@smoke",
+        dryRun = false*/
+}
